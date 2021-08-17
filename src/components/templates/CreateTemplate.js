de2bcef4211d3fe2b223f5d68d06ee56b9990026
StellaGreen <vickysoler5@gmail.com>
2021-08-17 15:32:26 +0200

    createteplate first

--- src/components/templates/CreateTemplate.js
+++ src/components/templates/CreateTemplate.js
@@ -1,6 +1,31 @@
-import { Heading } from "@chakra-ui/react"
+import { Button, Input, Heading } from '@chakra-ui/react'
+import { useState } from 'react'
+import axios from 'axios'
 
 const CreateTemplate = () => {
-  return <Heading>Create ComEth</Heading>
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async () => {
+    const res = await axios.post('http://192.168.0.10:3333', {
+      username: username,
+      hash: password,
+    })
+    console.log(res)
+  }
+  return (<>
+  <Heading>Create ComEth</Heading>
+  <Input
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={handleLogin}>Login</Button>
+  </>)
 }
 export default CreateTemplate
