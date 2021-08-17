9ad345298b4edfe7622f734c27182444574465a9
Benmissi-A <aminebenmissi@hotmail.fr>
2021-08-17 17:31:04 +0200

    push in progress

--- src/Dapp.js
+++ src/Dapp.js
@@ -6,7 +6,10 @@ import Login from "./components/pages/Login";
 import Home from "./components/pages/Home";
 import Vote from "./components/pages/Vote";
 import Proposition from "./components/pages/Proposition";
+import Info from "./components/pages/Info";
 import Stats from "./components/pages/Stats";
+import Budget from "./components/pages/Budget";
+import People from "./components/pages/People";
 
 
 const Dapp = () => {
@@ -16,28 +19,32 @@ const Dapp = () => {
           <Route exact path="/" component={LandingPage}>
             <LandingPage />
           </Route>
-
           <Route exact path="/Create" component={Create}>
             <Create />
           </Route>
-
           <Route exact path="/Login" component={Login}>
             <Login />
           </Route>
-
           <Route exact path="/Home" component={Home}>
             <Home />
           </Route>
-
           <Route exact path="/Vote" component={Vote}>
             <Vote />
           </Route>
-
           <Route exact path="/Proposition" component={Proposition}>
             <Proposition />
           </Route>
 
+          <Route exact path="/Info" component={Info}>
+            <Info />
+          </Route>
           <Route exact path="/Stats" component={Stats}>
+            <Stats />
+          </Route>
+          <Route exact path="/Budget" component={Budget}>
+            <Budget />
+          </Route>
+          <Route exact path="/People" component={People}>
             <Stats />
           </Route>
         </Switch>
