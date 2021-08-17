7fd71ad3f91072c0452e5bd07163557bce78c018
Benmissi-A <aminebenmissi@hotmail.fr>
2021-08-17 12:21:03 +0200

    add contract ComEthFactory

---
+++ src/contracts/comEthFactory.js
@@ -0,0 +1,111 @@
+comEthFactoryContract = "0xBf02E0D1A6c23BB14800e50390791a6792c93139";
+comEthFactoryAbi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "factoryOwner_",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "contract ComEth",
+        name: "comEth",
+        type: "address",
+      },
+    ],
+    name: "ComEthCreated",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "comEthAddresses",
+    outputs: [
+      {
+        internalType: "contract ComEth",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "comEthOwner_",
+        type: "address",
+      },
+    ],
+    name: "createComEth",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
